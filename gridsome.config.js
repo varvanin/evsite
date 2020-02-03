e63773e211704c5c032c441b1ffe921f47589da7
Yevhen Varvanin <varvanin84@gmail.com>
2020-02-03 21:48:41 +0100

    add logo

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome Blog Starter',
+  siteName: 'szybko. tanio. zajebiście.',
   siteDescription: 'A simple, hackable & minimalistic starter for Gridsome that uses Markdown for content.',
 
   templates: {
--- src/components/Homelogo.vue
+++ src/components/Homelogo.vue
@@ -1,22 +1,35 @@
 <template>
-	<div class="homelogo">
+  <div class="homelogo">
+    <div v-if="darkTheme">
+      <g-image
+        alt="homelogo image"
+        class="homelogo__image"
+        src="~/assets/images/ev_invert.svg"
+        width="180"
+        height="180"
+        blur="0"
+      />
+    </div>
+    <div v-else>
+      <g-image
+        alt="homelogo image"
+        class="homelogo__image"
+        src="~/assets/images/ev.svg"
+        width="180"
+        height="180"
+        blur="0"
+      />
+    </div>
 
-		<g-image alt="homelogo image" class="homelogo__image" src="~/assets/images/ev.svg" width="180" height="180" blur="0" />
+    <h1 v-if="showTitle" class="homelogo__site-title">{{ $static.metadata.siteName }}</h1>
 
-		<h1 v-if="showTitle" class="homelogo__site-title">
-			{{ $static.metadata.siteName }}
-		</h1>
+    <p class="homelogo__intro">Strone internetowe</p>
 
-		<p class="homelogo__intro">
-			A simple, hackable & minimalistic starter for Gridsome that uses Markdown for content.
-		</p>
-
-		<p class="homelogo__links">
-			<a href="//twitter.com/gridsome">Follow on Twitter</a>
-			<a href="//github.com/gridsome/gridsome-starter-blog">GitHub</a>
-		</p>
-
-	</div>
+    <p class="homelogo__links">
+      <a href="//twitter.com/gridsome">Follow on Twitter</a>
+      <a href="//github.com/gridsome/gridsome-starter-blog">GitHub</a>
+    </p>
+  </div>
 </template>
 
 <static-query>
@@ -29,37 +42,53 @@ query {
 
 <script>
 export default {
-	props: ['showTitle']
-}
+	data() {
+    return {
+      darkTheme: false
+    }
+  },
+  methods: {
+    toggleTheme() {
+      this.darkTheme = !this.darkTheme
+
+      // This is using a script that is added in index.html
+      window.__setPreferredTheme(
+        this.darkTheme ? 'dark' : 'light'
+      )
+    }
+  },
+  mounted() {
+    if (window.__theme == 'dark') this.darkTheme = true
+  },
+  props: ["showTitle"]
+};
 </script>
 
 <style lang="scss">
 .homelogo {
-	margin: 0 auto;
-	max-width: 500px;
-	text-align: center;
-	color: var(--body-color)!important;
-	padding: calc(var(--space) / 2) 0;
+  margin: 0 auto;
+  max-width: 500px;
+  text-align: center;
+  padding: calc(var(--space) / 2) 0;
 
-	&__image {
-		width: 300px;
-		height: 230px;
+  &__image {
+    width: 300px;
+    height: 230px;
+  }
 
-	}
+  &__intro {
+    opacity: 0.8;
+  }
 
-	&__intro {
-		opacity: .8;
-	}
+  &__site-title {
+    font-size: 1.5em;
+  }
 
-	&__site-title {
-		font-size: 1.5em;
-	}
-
-	&__links {
-		margin-top: -.5em;
-		a {
-			margin: 0 .5em;
-		}
-	}
+  &__links {
+    margin-top: -0.5em;
+    a {
+      margin: 0 0.5em;
+    }
+  }
 }
 </style>
