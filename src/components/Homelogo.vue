57e4b2e98924eb03ea278ce833bb6db0e2d5e9fb
Yevhen Varvanin <varvanin84@gmail.com>
2020-02-03 11:59:25 +0100

    Update Homelogo.vue

--- src/components/Homelogo.vue
+++ src/components/Homelogo.vue
@@ -1,17 +1,17 @@
 <template>
-	<div class="author">
+	<div class="homelogo">
 
 		<g-image alt="homelogo image" class="homelogo__image" src="~/assets/images/author.jpg" width="180" height="180" blur="5" />
 
-		<h1 v-if="showTitle" class="author__site-title">
+		<h1 v-if="showTitle" class="homelogo__site-title">
 			{{ $static.metadata.siteName }}
 		</h1>
 
-		<p class="author__intro">
+		<p class="homelogo__intro">
 			A simple, hackable & minimalistic starter for Gridsome that uses Markdown for content.
 		</p>
 
-		<p class="author__links">
+		<p class="homelogo__links">
 			<a href="//twitter.com/gridsome">Follow on Twitter</a>
 			<a href="//github.com/gridsome/gridsome-starter-blog">GitHub</a>
 		</p>
