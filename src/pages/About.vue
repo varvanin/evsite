3fab7dd1a8b3f6916622d6ccb5e39b0dd6019ffc
Yevhen Varvanin <varvanin84@gmail.com>
2020-02-04 17:07:15 +0100

    Update About.vue

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,10 +1,7 @@
 <template>
   <Layout :show-logo="true">
-    
-  
-    <g-link to="/about/">O Mnie</g-link>
-    <g-link to="/projects/">Projekty</g-link>
-    <g-link to="/contact/">Kontakt</g-link>
+   
+
     
   </Layout>
 </template>
@@ -15,4 +12,8 @@
 
 <script>
 
+  metaInfo: {
+    title: 'Hello, world!'
+  }
+}
 </script>
