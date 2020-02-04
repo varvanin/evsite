85950ce116df86e38e8b9e9a5810909bb06b35ce
Yevhen Varvanin <varvanin84@gmail.com>
2020-02-04 16:58:59 +0100

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,6 +2,9 @@
   <Layout :show-logo="false">
     <!-- Homelogo intro -->
     <Homelogo :show-title="true" />
+    <g-link to="/about/">O Mnie</g-link>
+    <g-link to="/projects/">Projekty</g-link>
+    <g-link to="/contact/">Kontakt</g-link>
     
   </Layout>
 </template>
