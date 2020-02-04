00385999b9fc34e6061f9de0d7df67a437f65eb1
Yevhen Varvanin <varvanin84@gmail.com>
2020-02-04 17:00:37 +0100

    Create About.vue

---
+++ src/pages/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <Layout :show-logo="true">
+    
+  
+    <g-link to="/about/">O Mnie</g-link>
+    <g-link to="/projects/">Projekty</g-link>
+    <g-link to="/contact/">Kontakt</g-link>
+    
+  </Layout>
+</template>
+
+<page-query>
+
+</page-query>
+
+<script>
+
+</script>
