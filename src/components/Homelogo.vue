0e3d73907ae1d66cf832a3906b5ba2dc6a307b6a
Yevhen Varvanin <varvanin84@gmail.com>
2020-02-04 11:17:39 +0100

    Update Homelogo.vue

--- src/components/Homelogo.vue
+++ src/components/Homelogo.vue
@@ -11,7 +11,7 @@
 
     <h1 v-if="showTitle" class="homelogo__site-title">{{ $static.metadata.siteName }}</h1>
 
-    <p class="homelogo__intro">Strone internetowe</p>
+    <p class="homelogo__intro">Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker</p>
 
     <p class="homelogo__links">
       <a href="//twitter.com/gridsome">Follow on Twitter</a>
