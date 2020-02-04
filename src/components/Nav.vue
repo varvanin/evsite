3f46eea177355d541bd6ecadc3e439d2a33c88b1
Yevhen Varvanin <varvanin84@gmail.com>
2020-02-04 17:05:40 +0100

    add nav

---
+++ src/components/Nav.vue
@@ -0,0 +1,25 @@
+<template>
+  <Layout :show-logo="false">
+    <!-- Homelogo intro -->
+    <Homelogo :show-title="true" />
+    
+    <nav />
+    
+  </Layout>
+</template>
+
+<page-query>
+
+</page-query>
+
+<script>
+import Homelogo from '~/components/Nav.vue'
+export default {
+  components: {
+    Nav
+  },
+  metaInfo: {
+    title: 'Hello, world!'
+  }
+}
+</script>
