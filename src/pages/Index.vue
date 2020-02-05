2a0209ac8703996e0f241d031fd159c73f4558ba
Yevhen Varvanin <varvanin84@gmail.com>
2020-02-05 10:32:16 +0100

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout :show-logo="false">
     <!-- Homelogo intro -->
-    <Homelogo :show-title="true" :key="componentKey" />
+    <Homelogo :show-title="true" />
      
     
   </Layout>
@@ -15,18 +15,8 @@
 import Homelogo from '~/components/Homelogo.vue'
 
 export default {
-data() {
-    return {
-      componentKey: 0
-    }
-  },  
   components: {
     Homelogo
-  },
-    methods: {
-    forceRerender() {
-      this.componentKey += 1
-    }
   },
   metaInfo: {
     title: 'Hello, world!'
