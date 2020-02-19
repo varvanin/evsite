eb0c65ab7e1e55c2a29cb6df007782043a452991
Yevhen Varvanin <varvanin84@gmail.com>
2020-02-19 22:15:47 +0100

    color change

--- src/components/Logotype.vue
+++ src/components/Logotype.vue
@@ -2,7 +2,6 @@
 
     <div class="logo-image">
         <svg width="300" clip-rule="evenodd" fill-rule="evenodd" version="1.1" viewBox="0 0 300 230" xml:space="preserve" xmlns="http://www.w3.org/2000/svg">
-
               <path class="logo-image" d="M23.9289+22.2507L72.3613+22.3669C77.4017+22.379+82.8905+26.0696+84.6209+30.6102L94.9082+57.6047C96.6386+62.1453+93.9552+65.8261+88.9148+65.8261L23.9289+65.8261C18.8884+65.8261+14.8024+61.9071+14.8024+57.0727L14.8024+30.9823C14.8024+26.1479+18.8884+22.2386+23.9289+22.2507Z" fill="var(--logo-color)" stroke="var(--logo-color)" stroke-linecap="round" opacity="1" stroke-linejoin="round"/>
               <path class="logo-image" d="M23.8847+92.1024L98.8665+92.6707C103.853+92.7085+109.263+96.3884+110.949+100.89L120.918+127.502C122.604+132.004+119.928+135.652+114.941+135.651L23.8849+135.633C18.8978+135.632+14.8549+131.754+14.8549+126.97L14.8549+100.695C14.8549+95.9116+18.8977+92.0646+23.8847+92.1024Z" fill="var(--logo-color)" stroke="var(--logo-color)" stroke-linecap="round" opacity="1" stroke-linejoin="round"/>
               <path class="logo-image" d="M23.9852+161.927L124.757+162.178C129.799+162.19+135.282+165.886+137.003+170.432L147.129+197.179C148.85+201.725+146.157+205.413+141.114+205.417L23.9852+205.495C18.9427+205.498+14.8549+201.58+14.8549+196.744L14.8549+170.661C14.8549+165.824+18.9427+161.914+23.9852+161.927Z" fill="var(--logo-color)" stroke="var(--logo-color)" stroke-linecap="round" opacity="1" stroke-linejoin="round"/>
